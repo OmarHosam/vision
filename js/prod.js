4524a6136d5f28c08f2534d0a0ea62e21bad5660
Omar Hosam <70144775+OmarHosam@users.noreply.github.com>
2021-06-24 16:21:45 +0200

    Delete js directory

--- js/prod.js
+++
@@ -1,8 +0,0 @@
-button = document.getElementById('btnSearch');
-search = document.getElementById('search');
-
-button.addEventListener("click", function (evt) {
- evt.preventDefault();
- window.location.replace("../result.html?s=" + search.value);
- return false; 
-});
